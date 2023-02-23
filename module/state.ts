c76b0ddd270e2807d2d7d34dd14e2e7c9d131f55
Muh Syahendra Anindyantoro <syahendra@privyid.tech>
2023-02-23 19:52:34 +0700

    add state global

---
+++ module/state.ts
@@ -0,0 +1,12 @@
+import { shallowRef, triggerRef } from "vue";
+
+export function createSignal(value, options) {
+  console.log(value, options);
+  const r = shallowRef(value);
+  const get = () => r.value;
+  const set = (v) => {
+    r.value = typeof v === "function" ? v(r.value) : v;
+    if (options?.equals === false) triggerRef(r);
+  };
+  return [get, set];
+}
